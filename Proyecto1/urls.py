a9a2a3ebfabb536c3ff9af996e8ae1b4bf048a62
Luis Antonio <luisantonio27@live.com.mx>
2020-08-07 16:06:40 -0500

    Paso de parametros en url

--- Proyecto1/urls.py
+++ Proyecto1/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Proyecto1.views import saludo, despedida
+from Proyecto1.views import saludo, despedida, dameFecha, calculaEdad
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
     path('hastaluego/', despedida),
+    path('fecha/', dameFecha),
+    path('edades/<int:edad>/<int:year>', calculaEdad)
 ]
--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
+import datetime
 
 def saludo(request): #primera vista
 	return HttpResponse("Primera p&aacute;gina con Django")
 
 def despedida(request):
 	return HttpResponse("Despedida de Django")
+
+def dameFecha(request):
+	fecha_actual = datetime.datetime.now()
+	return HttpResponse("Fecha y hora actuales: %s" %fecha_actual)
+
+def calculaEdad(request, edad, year):
+	periodo = year - datetime.datetime.now().year
+	edadFutura = edad + periodo
+	return HttpResponse("En el año %s tendras %s años" %(year, edadFutura))
