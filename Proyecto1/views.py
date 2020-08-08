a75163276070463c2c048d2342cb61e6bba92421
Luis Antonio <luisantonio27@live.com.mx>
2020-08-08 13:36:44 -0500

    Uso de diccionarios en el contexto

--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -2,11 +2,25 @@ from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
 
+class Persona(object):
+
+	def __init__(self, nombre, apellido):
+	    self.nombre = nombre
+	    self.apellido = apellido
+
 def saludo(request): #primera vista
+
+	p1 = Persona("Antonio", "Cruz")
+
+	nombre = "Luis"
+	apellido = "Cruz"
+	ahora = datetime.datetime.now()
+
 	doc_externo = open("Proyecto1/plantillas/miplantilla.html")
 	plantilla = Template(doc_externo.read())
 	doc_externo.close()
-	contexto = Context()
+	contexto = Context({"nombre_persona" : p1.nombre, "apellido_persona" : p1.apellido,
+		"momento_actual" : ahora})
 	documento = plantilla.render(contexto)
 	return HttpResponse(documento)
 
